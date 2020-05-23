ad6f7f16177d8b23ec571c1144e7c15242c325d3
MoonAuSosiGi::Desktop <moonausosigi@gmail.com>
2020-05-23 17:11:31 +0900

    * Unreal 초기 프로젝트 세팅

---
+++ Source/MoonAuRPGSample/MoonAuRPGSampleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MoonAuRPGSampleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOONAURPGSAMPLE_API AMoonAuRPGSampleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
